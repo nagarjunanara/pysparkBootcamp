2aa8177ad432feda193dbe9a8cd1f91fafee42a6
nagarjunanara <nagarjunanara@gmail.com>
2021-07-31 00:39:47 +0530

    Delete src directory

--- src/main/python/retail_db/Df/GettingStarted.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark=SparkSession.builder.master('local').appName('Dataframes_practice').getOrCreate()
-orderDF=spark.read.csv('C:\\Users\\nara_\\OneDrive\\Documents\\data-master\\data-master\\retail_db\\orders')
-orderDF.show()
-spark.read.jdbc("")
